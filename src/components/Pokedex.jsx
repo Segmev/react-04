20eae95a8126517c856895f39a4038da698e7551
Stéphane K <steffkarraz@hotmail.com>
2021-01-14 23:56:26 +0100

    Patron de l'application

---
+++ src/components/Pokedex.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Pokedex() {
+
+    return (
+        <>
+            <div>Image du pokémon</div>
+            <div>Nom du pokémon</div>
+            <div>Description du pokémon</div>
+            <div>Pokémon précédent - Pokémon suivant</div>
+            <div>Entrer un nom de pokémon</div>
+        </>
+    )
+}
