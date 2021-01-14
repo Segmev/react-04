38f29b9e9da437bcd8fbc98748c6bfc1d15d65db
Stéphane K <steffkarraz@hotmail.com>
2021-01-15 00:25:22 +0100

    Pokemon context created

---
+++ src/contexts/PokemonContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext } from "react";
+
+export const pokemonInitData = {
+    id: 0,
+    name: "",
+    picture: "",
+    description: "",
+}
+
+export const PokemonContext = createContext(pokemonInitData);
+
+export const PokemonContextProvider = PokemonContext.Provider;
+
+export const usePokemonContext = () => {
+    const context = useContext(PokemonContext);
+    if (context === undefined) {
+        throw new Error('The usePokemonContext is missing a Pokemon provider')
+    }
+    return context;
+}
+
+export default PokemonContext;
